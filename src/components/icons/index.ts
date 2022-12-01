e2c30fe768f5980f0a33436e28a11af03c5cf929
Ruchika30 <sood.ruchika@siesgst.ac.in>
2022-12-01 21:23:23 +0530

    updated icon paths

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,13 +1,13 @@
 
-export { default as MoreHorizIcon } from "@material-ui/icons/MoreHoriz";
-export { default as PermIdentityIcon } from "@material-ui/icons/PermIdentity";
-export { default as ListAltIcon } from "@material-ui/icons/ListAlt";
-export { default as BookmarkBorderIcon } from "@material-ui/icons/BookmarkBorder";
-export { default as MailOutlineIcon } from "@material-ui/icons/MailOutline";
-export { default as HomeIcon } from "@material-ui/icons/Home";
+export { default as MoreHorizIcon } from '@mui/icons-material/MoreHoriz';
+export { default as PermIdentityIcon } from '@mui/icons-material/PermIdentity';
+export { default as ListAltIcon } from '@mui/icons-material/ListAlt';
+export { default as BookmarkBorderIcon } from '@mui/icons-material/BookmarkBorder';
+export { default as MailOutlineIcon } from '@mui/icons-material/MailOutline';
+export { default as HomeIcon } from '@mui/icons-material/Home';
 export { default as TwitterIcon } from "@mui/icons-material/Twitter";
 export { default as TagIcon } from '@mui/icons-material/Tag';
-export { default as NotificationsNoneIcon } from "@material-ui/icons/NotificationsNone";
+export { default as NotificationsNoneIcon } from '@mui/icons-material/NotificationsNone';
 export { default as CloseIcon } from '@mui/icons-material/Close';
 export { default as SettingsIcon } from '@mui/icons-material/Settings';
 export { default as GoogleIcon } from '@mui/icons-material/Google';
